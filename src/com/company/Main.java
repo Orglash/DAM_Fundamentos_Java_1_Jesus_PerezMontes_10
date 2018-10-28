8931fdcb6d5c237833353cdb1f851d2326ee204a
Orglash <jesus.perez@escuelaestech.es>
2018-10-28 17:12:55 +0100

    Comentarios añadidos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,21 +5,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        //Variable booleana para el bucle while de mas adelante
         boolean condicion=false;
         double numero;
+        //Pedimos el numero y lo asignamos a la variable
         System.out.println("Introduzca un número entre 0 y 9.999:\n");
         Scanner sc=new Scanner(System.in);
         numero=sc.nextInt();
-
+        //Comprobamos que el numero introducido esta en el rango que hemos definido, y si no lo esta, volvemos a pedir un numero por consola
         while(numero<0||numero>9999) {
             System.out.println("Introduzca un número entre 0 y 9.999:\n");
             numero = sc.nextInt();
         }
-        double variable=10;
+        //Variable para comparar con el numero introducido
+        double compara=10;
+        //Variable contador, para tener el numero de cifras segun itere el bucle while.
         int contador=1;
+        //Con este bucle while comparamos nuestro numero con contador, y en caso de que el numero sea mayor, incrementamos contador y multiplicamos compara por 10
         while (condicion==false){
-            if(numero>=variable){
-                variable=variable*10;
+            if(numero>=compara){
+                compara=compara*10;
                 contador++;
             }else{
                 System.out.printf("El numero tiene %d cifras.", contador);
